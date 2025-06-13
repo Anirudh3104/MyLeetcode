39a706036210710b4905b43c190acbfd0e2c5e6a
Anirudh3104 <126007477+Anirudh3104@users.noreply.github.com>
2025-06-13 16:52:16 +0530

    Time: 47 ms (25.10%) | Memory: 30.7 MB (82.87%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -1,16 +1,20 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        n=len(nums)
-        i=0
-        res=[]
-        while i<n:
-            crt_idx=nums[i]-1
-            if nums[i]!=nums[crt_idx]:
-                nums[i],nums[crt_idx]=nums[crt_idx],nums[i]
-            else:
-                i+=1
-        for i in range(n):
-            if nums[i]!=i+1:
-                res.append(i+1)
-        return res
+        # n=len(nums)
+        # i=0
+        # res=[]
+        # while i<n:
+        #     crt_idx=nums[i]-1
+        #     if nums[i]!=nums[crt_idx]:
+        #         nums[i],nums[crt_idx]=nums[crt_idx],nums[i]
+        #     else:
+        #         i+=1
+        # for i in range(n):
+        #     if nums[i]!=i+1:
+        #         res.append(i+1)
+        # return res
+        for num in nums:
+            idx=abs(num)-1
+            nums[idx]=-abs(nums[idx])
+        return [i+1 for i in range(len(nums)) if  nums[i]>0]
         
