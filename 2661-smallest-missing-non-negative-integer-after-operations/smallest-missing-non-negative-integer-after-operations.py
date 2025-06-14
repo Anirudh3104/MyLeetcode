c5e5910667558d2fc8f94f0029f659efa29f458e
Anirudh3104 <126007477+Anirudh3104@users.noreply.github.com>
2025-06-14 12:18:11 +0530

    Time: 44 ms (97.28%) | Memory: 32.4 MB (86.39%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.py
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.py
@@ -1,19 +1,25 @@
 class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
-        mod={}
+        # mod={}
+        # for num in nums:
+        #     x=num%value
+        #     if x in mod:
+        #         mod[x]+=1
+        #     else:
+        #         mod[x]=1
+        # i=0 
+        # while True:
+        #     r=i%value
+        #     if mod.get(r, 0) > 0:
+        #         mod[r]-=1
+        #         i+=1
+        #     else:
+        #         return i
+        mod=[0]*value
         for num in nums:
-            x=num%value
-            if x in mod:
-                mod[x]+=1
-            else:
-                mod[x]=1
-        i=0 
-        while True:
-            r=i%value
-            if mod.get(r, 0) > 0:
-                mod[r]-=1
-                i+=1
-            else:
-                return i
-                    
+            mod[num%value]+=1
+        min_count=min(mod)
+        for i,count in enumerate(mod):
+            if count==min_count:
+                return min_count*value+i
         
