ea761687d061f9057f10e917c0a4c9248b7f452a
Anirudh3104 <126007477+Anirudh3104@users.noreply.github.com>
2025-09-21 16:19:45 +0530

    Time: 1 ms (40.61%) | Memory: 19.1 MB (50.49%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -1,13 +1,26 @@
 class Solution:
     import heapq
     def thirdMax(self, nums: List[int]) -> int:
-        nums=set(nums)
+        nums=list(set(nums))
         if len(nums)<3:
             return max(nums)
-        max_heap=[-x for x in nums]
-        heapq.heapify(max_heap)
-        for i in range(2):
-            heapq.heappop(max_heap)
-        return -heapq.heappop(max_heap)
+        # max_heap=[-x for x in nums]
+        # heapq.heapify(max_heap)
+        # for i in range(2):
+        #     heapq.heappop(max_heap)
+        # return -heapq.heappop(max_heap)
+        a,b,c=nums[0],float("-inf"),float("-inf")
+        for i in range(1,len(nums)):
+            if nums[i]>a:
+                c=b
+                b=a
+                a=nums[i]
+            elif nums[i]>b:
+                c=b
+                b=nums[i]
+            elif nums[i]>c:
+                c=nums[i]
+            else:pass
+        return c
 
         
