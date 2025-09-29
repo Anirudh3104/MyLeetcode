a1fe83fb16acd76dec07ccf9a572708effec85e1
Anirudh3104 <126007477+Anirudh3104@users.noreply.github.com>
2025-09-30 00:59:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (48.55%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,35 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        n=len(s)
+        def check(s,i,num):
+            if i>=n:return num
+            if s[i]==" ":
+                if num:return num
+                return check(s,i+1,num) 
+            elif  s[i]=="-":
+                if not num:
+                    return check(s,i+1,num+s[i])
+                else:return num
+            elif s[i]=="+":
+                if not num:
+                    return check(s,i+1,num+s[i])
+                return num
+            elif s[i].isdigit():
+                return check(s,i+1,num+s[i])
+            else:return num
+             
+        x=check(s,0,'')
+        if not x or x in ('+','-'):
+            return 0
+        minn,maxx=-2**31,2**31-1
+        x=int(x)
+        if x>maxx:return maxx
+        if x<minn:return minn
+        return x
+            
+        
+
+
+                
+
+        
